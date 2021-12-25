5cba34439d54da22465c17caee1fb9fb2f496a02
Ankush <ankush357159@gmail.com>
2021-12-25 13:41:35 +0530

    login api

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,16 @@
 import React from "react";
-import { Nav, Navbar } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { Link, useHistory } from "react-router-dom";
 
 export const Header = () => {
+  const history = useHistory();
+  const user = JSON.parse(localStorage.getItem("user-info"));
+  // console.log(user);
+
+  const logout = () => {
+    localStorage.clear();
+    history.push("/register");
+  };
   return (
     <Navbar bg='dark' expand='lg'>
       <Navbar.Brand href='#home'>Navbar</Navbar.Brand>
@@ -19,6 +27,14 @@ export const Header = () => {
           </>
         )}
       </Nav>
+      {localStorage.getItem("user-info") ? (
+        <Nav>
+          <NavDropdown title={user && user.name}>
+            <NavDropdown.Item onClick={logout}>Logout</NavDropdown.Item>
+            <NavDropdown.Item>Profile</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+      ) : null}
     </Navbar>
   );
 };
--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,27 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { Header } from "./Header";
 
 const Login = (props) => {
   const history = useHistory();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = async () => {
+    console.log("Data: ", email, password);
+    let item = { email, password };
+    let result = await fetch("http://localhost:8000/api/login/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+      body: JSON.stringify(item),
+    });
+    result = await result.json();
+    localStorage.setItem("user-info", JSON.stringify(result));    
+    history.push("/add");
+  };
 
   useEffect(() => {
     if (localStorage.getItem("user-info")) {
@@ -13,7 +31,26 @@ const Login = (props) => {
   return (
     <div>
       <Header />
-      <h2>Login Page</h2>
+      <div className='col-sm-6 offset-sm-3'>
+        <h2>Login Page</h2>
+        <input
+          type='text'
+          className='form-control'
+          placeholder='Enter your email'
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <br />
+        <input
+          type='text'
+          className='form-control'
+          placeholder='Enter your password'
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br />
+        <button className='btn btn-primary' onClick={login}>
+          Login
+        </button>
+      </div>
     </div>
   );
 };

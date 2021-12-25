d45591493c0aab4ef09016f9ff7634840bb26b74
Ankush <ankush357159@gmail.com>
2021-12-25 21:02:53 +0530

    Create Product api

--- src/AddProduct.js
+++ src/AddProduct.js
@@ -1,12 +1,67 @@
-import React from 'react'
-import { Header } from './Header'
+import React, { useState } from "react";
+import { Header } from "./Header";
 
 const AddProduct = () => {
-    return (
-        <div>
-            <Header />
-            <h2>Add Product Page</h2>
-        </div>
-    )
-}
-export default AddProduct
+  const [name, setName] = useState("");
+  const [file, setFile] = useState("");
+  const [price, setPrice] = useState("");
+  const [description, setDescription] = useState("");
+
+  const addProducts = async () => {
+    console.log(name, file, price, description);
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("name", name);
+    formData.append("price", price);
+    formData.append("description", description);
+
+    let result = await fetch("http://localhost:8000/api/addproduct/", {
+      method: "POST",
+      body: formData,
+    });
+    console.log("Data saved successfully", result);
+  };
+
+  return (
+    <div>
+      <Header />
+      <h2>Add Product Page</h2>
+      <div className='col-sm-6 offset-sm-3'>
+        <br />
+        <input
+          type='text'
+          className='form-control'
+          placeholder='Enter product name'
+          onChange={(e) => setName(e.target.value)}
+        />{" "}
+        <br />
+        <input
+          type='file'
+          className='form-control'
+          placeholder='Enter product file'
+          onChange={(e) => setFile(e.target.files[0])}
+        />{" "}
+        <br />
+        <input
+          type='text'
+          className='form-control'
+          placeholder='Enter product price'
+          onChange={(e) => setPrice(e.target.value)}
+        />{" "}
+        <br />
+        <input
+          type='text'
+          className='form-control'
+          placeholder='Enter product description'
+          onChange={(e) => setDescription(e.target.value)}
+        />{" "}
+        <br />
+        <button className='btn btn-primary' onClick={addProducts}>
+          Add Product
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default AddProduct;
---
+++ src/ProductList.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from "react";
+import { Table } from "react-bootstrap";
+import { Header } from "./Header";
+
+const ProductList = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      let result = await fetch("http://localhost:8000/api/list/");
+      result = await result.json();
+      setData(result);
+    }
+    fetchData();
+  }, []);
+    console.warn(data);
+
+//   useEffect(async () => {
+//     let result = await fetch("http://localhost:8000/api/list/");
+//     result = await result.json();
+//     setData(result);
+//   }, []);
+//   console.warn(data);
+
+  return (
+    <div>
+      <Header />
+      <div className='col-sm-10 offset-sm-1'>
+        <h2>Product List</h2>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Image</th>
+              <th>Description</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item) => (
+              <tr key={item.id}>
+                <td>{item.id}</td>
+                <td>{item.name}</td>
+                <td>
+                  <img
+                    style={{ width: 80 }}
+                    src={"http://localhost:8000/" + item.file_path}
+                    alt=''
+                  />
+                </td>
+                <td>{item.description}</td>
+                <td>Rs.{item.price}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
+    </div>
+  );
+};
+
+export default ProductList;
+
+// useEffect(() => {
+//     async function fetchData() {
+//       // You can await here
+//       const response = await MyAPI.getData(someId);
+//       // ...
+//     }
+//     fetchData();
+//   }, [someId]);
